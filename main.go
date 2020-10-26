6ec13c75c8fdb4e8c6c777f9713d482412ab672f
Maxim Valiarovski <maxim.valiarovski@target.com>
2020-10-26 17:26:45 -0500

    add vela variables

--- main.go
+++ main.go
@@ -20,42 +20,42 @@ func main() {
 		cli.StringFlag{
 			Name:   "cluster-config-path",
 			Usage:  "Path to the cluster config",
-			EnvVar: "CLUSTER_CONFIG,PLUGIN_CLUSTER_CONFIG",
+			EnvVar: "CLUSTER_CONFIG,PLUGIN_CLUSTER_CONFIG,PARAMETER_CLUSTER_CONFIG",
 		},
 		cli.StringSliceFlag{
 			Name:   "value-files",
 			Usage:  "Helm value override files",
-			EnvVar: "VALUE_FILES,PLUGIN_VALUE_FILES",
+			EnvVar: "VALUE_FILES,PLUGIN_VALUE_FILES,PARAMETER_VALUE_FILES",
 		},
 		cli.StringFlag{
 			Name:   "kube-config",
 			Usage:  "Kubernetes configuration file",
-			EnvVar: "KUBE_CONFIG,PLUGIN_KUBE_CONFIG",
+			EnvVar: "KUBE_CONFIG,PLUGIN_KUBE_CONFIG,PARAMETER_KUBE_CONFIG",
 		},
 		cli.StringFlag{
 			Name:   "kube-context",
 			Usage:  "Kubernetes configuration context to use",
-			EnvVar: "KUBE_CONTEXT,PLUGIN_KUBE_CONTEXT",
+			EnvVar: "KUBE_CONTEXT,PLUGIN_KUBE_CONTEXT,PARAMETER_KUBE_CONTEXT",
 		},
 		cli.BoolFlag{
 			Name:   "dry-run",
 			Usage:  "Enables a dry_run deployment",
-			EnvVar: "DRY_RUN,PLUGIN_DRY_RUN",
+			EnvVar: "DRY_RUN,PLUGIN_DRY_RUN,PARAMETER_DRY_RUN",
 		},
 		cli.BoolFlag{
 			Name:   "diff-run",
 			Usage:  "compares upgrade changes deployment",
-			EnvVar: "DIFF_RUN,PLUGIN_DIFF_RUN",
+			EnvVar: "DIFF_RUN,PLUGIN_DIFF_RUN,PARAMETER_DIFF_RUN",
 		},
 		cli.BoolFlag{
 			Name:   "audit",
 			Usage:  "create audit report",
-			EnvVar: "AUDIT_RUN,PLUGIN_AUDIT_RUN",
+			EnvVar: "AUDIT_RUN,PLUGIN_AUDIT_RUN,PARAMETER_AUDIT_RUN",
 		},
 		cli.StringFlag{
 			Name:   "audit-file",
 			Usage:  "audit report file name",
-			EnvVar: "AUDIT_FILE_NAME,PLUGIN_AUDIT_FILE_NAME",
+			EnvVar: "AUDIT_FILE_NAME,PLUGIN_AUDIT_FILE_NAME,PARAMETER_AUDIT_FILE_NAME",
 		},
 	}
 
