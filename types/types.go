aa2f77bcd04a406ef8eaf32dc78fa43038821b75
Kannan Thiruneelakandan <kannan.iitm@gmail.com>
2026-02-25 14:52:41 +0530

    Update types.go

--- types/types.go
+++ types/types.go
@@ -23,15 +23,20 @@ type HelmRepo struct {
 }
 
 type Release struct {
-	Name             string     `yaml:"name"`
-	DeploymentMethod string     `yaml:"deploymentMethod,omitempty"`
-	Version          string     `yaml:"version"`
-	ChartPath        string     `yaml:"chartPath"`
-	ChartsSource     string     `yaml:"chartsSource"`
-	History          uint       `yaml:history`
-	Overrides        []Override `yaml:"overrides,omitempty"`
-	Namespace        string     `yaml:"namespace,omitempty"`
-	ValueFiles       []string   `yaml:"valueFiles,omitempty"`
+	Name                string     `yaml:"name"`
+	DeploymentMethod    string     `yaml:"deploymentMethod,omitempty"`
+	Version             string     `yaml:"version"`
+	ChartPath           string     `yaml:"chartPath"`
+	ChartsSource        string     `yaml:"chartsSource"`
+	History             uint       `yaml:history`
+	Overrides           []Override `yaml:"overrides,omitempty"`
+	Namespace           string     `yaml:"namespace,omitempty"`
+	ValueFiles          []string   `yaml:"valueFiles,omitempty"`
+	WaitforDeployment   []string   `yaml:"waitforDeployment,omitempty"`
+	WaitforDaemonSet    []string   `yaml:"waitforDaemonSet,omitempty"`
+	WaitforStatefulSet  []string   `yaml:"waitforStatefulSet,omitempty"`
+	KubectlFiles        []string   `yaml:"kubectlFiles,omitempty"`
+	Force               bool       `yaml:"force,omitempty"`
 }
 
 type Override struct {
@@ -61,6 +66,84 @@ type HelmConfig struct {
 
 type Value struct {
 	Value     *string    `yaml:"value,omitempty"`
+	ValueFrom *ValueFrom `yaml:"valueFrom,omitempty"`
+	ShowValue bool       `yaml:"showValue"`
+}
+
+func (v Value) BuildArg(name string) (*commandbuilder.Arg, error) {
+	if v.Value != nil {
+		if *v.Value == "" {
+			log.Println("WARNING: Override value is blank.")
+		}
+		return &commandbuilder.Arg{
+			Type:        commandbuilder.ArgTypeLongParam,
+			Name:        "set",
+			Value:       fmt.Sprintf("%s=%s", name, *v.Value),
+			ValueSecret: !v.ShowValue,
+		}, nil
+	} else if v.ValueFrom != nil {
+		return v.ValueFrom.BuildArg(name, v.ShowValue)
+	} else {
+		return nil, fmt.Errorf("no value provided")
+	}
+}
+
+// GetValue returns the override value as a string. If a file or environment
+// variable is set, the contents are read and returned as a string.
+func (v Value) GetValue() (string, error) {
+	if v.Value != nil {
+		return *v.Value, nil
+	}
+	if v.ValueFrom != nil {
+		return v.ValueFrom.GetValue()
+	}
+	return "", fmt.Errorf("no value provided")
+}
+
+type ValueFrom struct {
+	Environment string `yaml:"environment"`
+	File        string `yaml:"file"`
+}
+
+func (vf ValueFrom) BuildArg(name string, show bool) (*commandbuilder.Arg, error) {
+	if vf.Environment != "" {
+		value, err := vf.GetValue()
+		if err != nil {
+			return nil, err
+		}
+		if value == "" {
+			log.Println("WARNING: Override value is blank.")
+		}
+		return &commandbuilder.Arg{
+			Type:        commandbuilder.ArgTypeLongParam,
+			Name:        "set",
+			Value:       fmt.Sprintf("%s=%s", name, value),
+			ValueSecret: !show,
+		}, nil
+	} else if vf.File != "" {
+		return &commandbuilder.Arg{
+			Type:        commandbuilder.ArgTypeLongParam,
+			Name:        "set-file",
+			Value:       fmt.Sprintf("%s=%s", name, vf.File),
+			ValueSecret: false,
+		}, nil
+	} else {
+		return nil, fmt.Errorf("no source specified for ValueFrom")
+	}
+}
+
+func (vf ValueFrom) GetValue() (string, error) {
+	if vf.Environment != "" {
+		return os.Getenv(vf.Environment), nil
+	} else if vf.File != "" {
+		bytes, err := ioutil.ReadFile(vf.File)
+		if err != nil {
+			return "", err
+		}
+		return string(bytes), nil
+	}
+	return "", fmt.Errorf("no source specified for ValueFrom")
+}
 	ValueFrom *ValueFrom `yaml:"valueFrom,omitempty"`
 	ShowValue bool       `yaml:"showValue"`
 }
